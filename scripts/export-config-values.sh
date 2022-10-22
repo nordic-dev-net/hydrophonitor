7a91d90a7cb78f39eabb17a36332e2aabdb7a2c2
Satu Koskinen <satu.a.koskinen@gmail.com>
2022-10-22 15:56:41 +0300

    Add option to pass CONFIG_FILE as parameter to export-config-values.sh

--- scripts/export-config-values.sh
+++ scripts/export-config-values.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-CONFIG_FILE=/boot/hydrophonitor/configuration/hydrophonitor-config.txt
+if [ $# -eq 1 ]; then
+	CONFIG_FILE=$1
+else
+	CONFIG_FILE=/boot/hydrophonitor/hydrophonitor-config.txt
+fi
 
 export $(grep -v '^#' $CONFIG_FILE | tr -d '[:space:]' | xargs -d '\n')
