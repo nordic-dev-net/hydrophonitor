8f98117aee91117aeb24ae525cc35dc05ad9dad9
Satu Koskinen <satu.a.koskinen@gmail.com>
2023-10-16 14:10:32 +0200

    Implement configuration options for shutdown-button service (#33)
    - GPIO pin
    - button press duration in seconds to trigger shutdown
    - add logging
    
    Signed-off-by: Satu Koskinen <satu.a.koskinen@gmail.com>

--- modules/shutdown-button/src/main.rs
+++ modules/shutdown-button/src/main.rs
@@ -1,25 +1,48 @@
-//! Display message in console when a Button is pressed
-// use rust_gpiozero::{Button, Debounce};
-use rust_gpiozero::{Button};
-// use std::time::Duration;
+use clap::Parser;
+use log::info;
+use rust_gpiozero::Button;
 use std::process::Command;
+use std::time::Instant;
+
+#[derive(Parser)]
+#[clap(version = "1.0", author = "Satu Koskinen")]
+struct Cli {
+    /// GPIO pin that the button is connected to
+    #[clap()]
+    gpio: u8,
+
+    /// Button press duration in seconds to trigger shutdown
+    #[clap()]
+    duration: u32,
+}
 
 fn main() {
-    // Create a button which is attached to Pin 21
-    let mut button = Button::new(21);
-        // Add debouncing so that subsequent presses within 100ms don't trigger a press
-        // .debounce(Duration::from_millis(100));
+    env_logger::init();
+    let args = Cli::parse();
 
-    // Add an async interrupt to trigger whenever the button is pressed
-    // button
-    //     .when_pressed(|_| {
-    //         println!("button pressed");
-    //     })
-    //     .unwrap();
-    println!("Waiting for shutdown button press");
-    button.wait_for_press(None);
-    println!("Shutdown button pressed");
-    Command::new("shutdown")
-        .output()
-        .expect("failed to execute shutdown command");
+    info!("Starting shutdown button on GPIO {}", args.gpio);
+    let mut button = Button::new(args.gpio);
+
+    loop {
+        info!(
+            "Waiting for shutdown button press, press for longer than {:?} to shutdown",
+            args.duration
+        );
+        button.wait_for_press(None);
+        let press_start = Instant::now();
+        info!("Shutdown button pressed");
+        button.wait_for_release(None);
+        let duration = press_start.elapsed();
+        info!("Shutdown button released after {:?}", duration);
+        if duration.as_secs_f32() > args.duration as f32 {
+            info!(
+                "Shutdown button pressed for longer than {:?}, shutting down",
+                args.duration
+            );
+            Command::new("shutdown")
+                .output()
+                .expect("failed to execute shutdown command");
+            break;
+        }
+    }
 }
